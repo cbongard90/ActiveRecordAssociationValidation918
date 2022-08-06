fc5e4c6df3d31fea1918004e1320a6807eafc895
Christian Bongard <christian.bongard90@gmail.com>
2022-08-06 11:35:52 +0400

    added references and validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
 class Doctor < ActiveRecord::Base
   has_many :interns
   has_many :consultations
+  # to give relationship with the patients table
+  has_many :patients, through: :consultations
+  # Adds a validation that a doctor MUST have a last name
+  validates :last_name, presence: true
 end
---
+++ db/migrate/20220806105404_remove_age_to_patients.rb
@@ -0,0 +1,5 @@
+class RemoveAgeToPatients < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :patients, :age, :integer
+  end
+end
